1f5214f9a44714bbf30ef11588f22fe2d290a549
MrPR270 <prahalad.ramkumar270@gmail.com>
2026-03-03 15:41:36 -0500

    Add video player functionality with dynamic playlist

---
+++ script.js
@@ -0,0 +1,43 @@
+/* script.js */
+const API_KEY = 'PASTE_YOUR_KEY_HERE'; // 1. Put your key here
+const player = new Plyr('#player');
+
+// FUNCTION TO EXTRACT ID FROM URL AUTOMATICALLY
+function extractId(input) {
+    if (!input.includes('/')) return input;
+    const regex = /\/d\/([a-zA-Z0-9_-]{25,})/;
+    const match = input.match(regex);
+    return match ? match[1] : input;
+}
+
+// DYNAMIC PLAYLIST LOADER
+fetch('playlist.json')
+    .then(res => res.json())
+    .then(data => {
+        const container = document.getElementById('playlist-container');
+        container.innerHTML = '';
+        data.forEach(item => {
+            const div = document.createElement('div');
+            div.className = 'item';
+            div.tabIndex = 0; 
+            div.innerText = item.title;
+            
+            div.onclick = () => {
+                const cleanId = extractId(item.id);
+                playVideo(cleanId, item.subs);
+            };
+            
+            div.onkeydown = (e) => { if(e.key === "Enter") div.onclick(); };
+            container.appendChild(div);
+        });
+    });
+
+function playVideo(fileId, subsPath) {
+    const videoSrc = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${API_KEY}`;
+    player.source = {
+        type: 'video',
+        sources: [{ src: videoSrc, type: 'video/mp4' }],
+        tracks: [{ kind: 'captions', label: 'English', srclang: 'en', src: subsPath, default: true }]
+    };
+    player.play();
+}
